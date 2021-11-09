8450b9d9fff21a941c495e9f36885b438b1a038f
urgora <91369295+urgora@users.noreply.github.com>
2021-11-09 18:55:47 +0530

    new map

--- Assets/scripts/rotation.cs
+++ Assets/scripts/rotation.cs
@@ -8,15 +8,15 @@ public class rotation : MonoBehaviour
     GameObject nextpoint;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.layer==6)
+        if (collision.gameObject.layer == 6)
         {
-            nextpoint = collision.gameObject;
+             nextpoint = collision.gameObject;
         }
     }
     private void Update()
     {
-        if(nextpoint!=null)
-        train.transform.rotation = Quaternion.Lerp(train.transform.rotation, nextpoint.gameObject.transform.rotation, 1f);
+        if (nextpoint != null)
+            train.transform.rotation = Quaternion.Lerp(train.transform.rotation, nextpoint.gameObject.transform.rotation, 1f);
     }
 
 }
